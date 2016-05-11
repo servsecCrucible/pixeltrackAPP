08bd57f7225ad534aeb7249404b2ca1daf003ec9
rjollet- <renaudjollet@gmail.com>
2016-05-11 16:59:12 +0800

    add more config in config env as app host

--- services/find_authenticated_account.rb
+++ services/find_authenticated_account.rb
@@ -3,7 +3,7 @@ require 'http'
 # returns authenticated user or nil
 class FindAuthenticatedAccount
 	def self.call(username:, password:)
-		response = HTTP.get("#{ENV['HOST']}/accounts/#{username}/authenticate",
+		response = HTTP.get("#{ENV['API_HOST']}/accounts/#{username}/authenticate",
 							params: {password: password})
 		response.code == 200 ? JSON.parse(response) : nil
 	end
