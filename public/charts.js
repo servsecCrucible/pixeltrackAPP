a78f3798706fde0d73f3dd4e6efd4125c15af5ed
Jherez Taylor <jherez.taylor@gmail.com>
2016-06-17 02:22:54 +0800

    Pies are working, time series does not, throwing an error

--- public/charts.js
+++ public/charts.js
@@ -1,86 +1,136 @@
 $(function() {
-    // var arr = new Array();
-    //     // campaign_set.tracker_set.forEach(function (item) {
-    //     //   arr.push(item);
-    //     // })
-    //     console.log(visit_set);
-    //     // console.log(arr);
-    //     console.log(campaign_set);
-    // campaign_set.forEach(function (item) {
-    //       console.log(item.id);
-    //     });
-    // console.log(cvisit_set);
     console.log("Hi");
-    // console.log(extractVisits(visit_set));
-    var visit_array = extractVisits(visit_set);
-    var res = getTotals(visit_array, 'hey');
-    // console.log(res);
-    // console.log(getTotals(visit_array,'hey'));
-    $('#container0').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: 1, //null,
-            plotShadow: false
-        },
-        title: {
-            text: 'Production breakdown by volume'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b> {point.y} Kg</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name} </b>: {point.percentage:.1f} % '
-                }
+    var count = 0;
+    var filter = ['os', 'ip', 'date'];
+    // console.log(visit_set);
+    $("#redraw_chart").click(function() {
+        parseData('time');
+    });
+    parseData('pie');
+
+
+    function parseData(param) {
+        _.forEach(visit_set, function(value, key) {
+            var visits = extractVisits(value);
+            if (param == 'pie') {
+                var res = getTotals(visits, filter);
+                setupPieChart(0, res.length, res);
+            } else if (param == 'time') {
+                var res = getVisitsByTime(visits);
+                setupTimeChart(0, res.length, res);
             }
-        },
-        series: [{
-            type: 'pie',
-            name: 'Production Output',
-            data: []
-        }]
-    });
-    chart = $('#container0').highcharts();
-    chart.series[0].setData(res);
-});
-
-function extractVisits(dataset) {
-    var res = new Array();
-    dataset.forEach(function(item) {
-        item.data.forEach(function(x) {
-            res.push(x.attributes);
         });
-    });
-    return res;
-}
+    }
 
-function getTotals(dataset, filter) {
-    console.log(filter);
-    var res = {};
-    var hey = [];
-    // console.log(dataset);
-    res.os = _.countBy(dataset, function(o) {
-        return o.os;
-    });
-    res.ip = _.countBy(dataset, function(o) {
-        return o.ip;
-    });
-    _.forEach(res.os, function(value, key) {
-        hey.push([key, value]);
-    })
-    console.log(hey);
-    // console.log(_.countBy(dataset,dataset.os));
-    // _.times(4, function(){
-    //   console.log('boy');
-    // })
-    // var res = new Array();
-    // dataset.forEach(function(item){
-    //   // console.log(item);
-    //   res.push(item);
-    // });
-    return hey;
-}
+    function setupPieChart(count, n, dataset) {
+        // console.log(dataset);
+        _.times(n, function() {
+            $('#container' + count).empty();
+            $('#container' + count).highcharts({
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: 1, //null,
+                    plotShadow: false
+                },
+                title: {
+                    text: 'Visit Breakdown by OS'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b> {point.y}</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name} </b>: {point.percentage:.1f} % '
+                        }
+                    }
+                },
+                series: [{
+                    type: 'pie',
+                    name: '# of vists',
+                    data: []
+                }]
+            });
+            chart = $('#container' + count).highcharts();
+            chart.series[0].setData(dataset);
+            count++;
+        });
+
+    }
+
+    function setupTimeChart(count, n, dataset) {
+        // console.log(dataset);
+        _.times(n, function() {
+            // $('#container' + count).empty();
+            $('#container' + count).highcharts('StockChart', {
+                loading: {
+                    hideDuration: 1000,
+                    showDuration: 1000
+                },
+                rangeSelector: {
+                    selected: 1,
+                    inputEnabled: $('#container' + count).width() > 480
+                },
+
+                tooltip: {
+                    pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>${point.y}</b><br/>',
+                    valueDecimals: 2
+                },
+                colors: ['#4D90FE', '#1BC123'],
+                title: {
+                    text: 'Visits by TIme'
+                },
+
+                chart: {},
+                series: [{
+                    data: []
+                }]
+            });
+
+            var sortedData = _.sortBy(dataset, function(num) {
+                return num;
+            });
+            console.log(sortedData);
+            chart = $('#container' + count).highcharts('StockChart');
+            chart.series[0].setData(sortedData);
+            chart.series[0].name = 'yea';
+        });
+
+    }
+
+    function extractVisits(dataset) {
+        var res = new Array();
+        _.forEach(dataset.data, function(value, key) {
+            res.push(value.attributes);
+        });
+        return res;
+    }
+
+    function getTotals(dataset, filter) {
+        var res = [];
+        res.os = _.countBy(dataset, function(o) {
+            return o[filter[0]];
+        });
+        // res.ip = _.countBy(dataset, function(o) {
+        //     return o[filter[1]];
+        // });
+        _.forEach(res.os, function(value, key) {
+            res.push([key, value]);
+        })
+        return res;
+    }
+
+    function getVisitsByTime(dataset) {
+        var res = [];
+        res.date = _.countBy(dataset, function(o) {
+            return o['date'];
+        });
+        _.forEach(res.date, function(value, key) {
+            res.push([parseInt(key,10), value]);
+        })
+        return res;
+    }
+});
