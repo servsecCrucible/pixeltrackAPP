2158239d1d244301e43a4ff9f9da247c2ac62ccb
Jherez Taylor <jherez.taylor@gmail.com>
2016-06-16 23:08:21 +0800

    first chart working

--- public/charts.js
+++ public/charts.js
@@ -9,53 +9,78 @@ $(function() {
     // campaign_set.forEach(function (item) {
     //       console.log(item.id);
     //     });
-    console.log(cvisit_set);
+    // console.log(cvisit_set);
     console.log("Hi");
+    // console.log(extractVisits(visit_set));
+    var visit_array = extractVisits(visit_set);
+    var res = getTotals(visit_array, 'hey');
+    // console.log(res);
+    // console.log(getTotals(visit_array,'hey'));
     $('#container0').highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: 1, //null,
+            plotShadow: false
+        },
         title: {
-            text: 'Monthly Average Temperature',
-            x: -20 //center
-        },
-        subtitle: {
-            text: 'Source: WorldClimate.com',
-            x: -20
-        },
-        xAxis: {
-            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-            ]
-        },
-        yAxis: {
-            title: {
-                text: 'Temperature (°C)'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#808080'
-            }]
+            text: 'Production breakdown by volume'
         },
         tooltip: {
-            valueSuffix: '°C'
+            pointFormat: '{series.name}: <b> {point.y} Kg</b>'
         },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
-            borderWidth: 0
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name} </b>: {point.percentage:.1f} % '
+                }
+            }
         },
         series: [{
-            name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-        }, {
-            name: 'New York',
-            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-        }, {
-            name: 'Berlin',
-            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-        }, {
-            name: 'London',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+            type: 'pie',
+            name: 'Production Output',
+            data: []
         }]
     });
+    chart = $('#container0').highcharts();
+    chart.series[0].setData(res);
 });
+
+function extractVisits(dataset) {
+    var res = new Array();
+    dataset.forEach(function(item) {
+        item.data.forEach(function(x) {
+            res.push(x.attributes);
+        });
+    });
+    return res;
+}
+
+function getTotals(dataset, filter) {
+    console.log(filter);
+    var res = {};
+    var hey = [];
+    // console.log(dataset);
+    res.os = _.countBy(dataset, function(o) {
+        return o.os;
+    });
+    res.ip = _.countBy(dataset, function(o) {
+        return o.ip;
+    });
+    _.forEach(res.os, function(value, key) {
+        hey.push([key, value]);
+    })
+    console.log(hey);
+    // console.log(_.countBy(dataset,dataset.os));
+    // _.times(4, function(){
+    //   console.log('boy');
+    // })
+    // var res = new Array();
+    // dataset.forEach(function(item){
+    //   // console.log(item);
+    //   res.push(item);
+    // });
+    return hey;
+}
